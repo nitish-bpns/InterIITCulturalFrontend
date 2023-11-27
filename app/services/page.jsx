9f2cd982f5fd497aec6908d45d450298a071235e
Saharsh Agrawal <saharshagrawal@gmail.com>
2023-11-27 16:06:23 +0530

    services page plank system

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,21 +1,32 @@
 import Styles from "./Services.module.css";
 import { Cookie } from "next/font/google";
 import Image from "next/image";
-import Plank from "../../public/assets/images/plank_services.png";
+
+import ServicesPlank from "../../public/assets/images/services/services.png";
+import FoodPlank from "../../public/assets/images/services/food.png";
+import CyclesPlank from "../../public/assets/images/services/cycles.png";
 
 const cookie = Cookie({ weight: "400", subsets: ["latin"] });
 
-export default function Sercices() {
-	return (
-		<main className={cookie.className}>
-			<section className={Styles["main"]}>
-				<div className={Styles["plank-container"]}>
-					<Image src={Plank} alt="Services" />
-					<h1 className={Styles["services"]}>Services</h1>
-					<h2 className={Styles["food"]}>Food</h2>
-					<h2 className={Styles["cycles"]}>Cycles</h2>
-				</div>
-			</section>
-		</main>
-	);
+export default function Services() {
+  return (
+    <main className={cookie.className}>
+      <section className={Styles["main"]}>
+        <div className={Styles["plank-container"]}>
+          <div className={Styles["food"]}>
+            <Image src={FoodPlank} alt="Food" />
+            <h1>Food</h1>
+          </div>
+          <div className={Styles["services"]}>
+            <Image src={ServicesPlank} alt="Services" />
+            <h1>Services</h1>
+          </div>
+          <div className={Styles["cycles"]}>
+            <Image src={CyclesPlank} alt="Cycles" />
+            <h1>Cycles</h1>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
