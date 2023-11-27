7d15579a68ee422ae59fbc511cf9328add2cf0cc
Saharsh Agrawal <saharshagrawal@gmail.com>
2023-11-27 19:06:08 +0530

    navbar and footer responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,86 @@
+"use client";
+
 import Link from "next/link";
 import localFont from "next/font/local";
 import Image from "next/image";
 import Styles from "./Navbar.module.css";
 import ILU from "../public/assets/images/ILU.png";
+import hamburger from "../public/assets/images/hamburger.png";
+import { useState } from "react";
 
 const myFont = localFont({ src: "../public/assets/fonts/Dreaming.woff2" });
 
 export default function Navbar() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
   return (
     <nav className={`${Styles["navbar"]} ${myFont.className}`}>
-      <Link href="/">
-        <Image src={ILU} alt="Logo" className={Styles["navbar-logo"]} />
-      </Link>
-      <div className={Styles["navbar-nav"]}>
-        <Link href="/events">Events</Link>
-        <Link href="/campus-map">Campus Map</Link>
-        <Link href="/leaderboard">Leaderboard</Link>
-        <Link href="/schedule">Schedule</Link>
-        <Link href="/signin">Sign In</Link>
+      <div className={Styles["navbar-header"]}>
+        <Link href="/">
+          <Image src={ILU} alt="Logo" className={Styles["navbar-logo"]} />
+        </Link>
+        <Image
+          src={hamburger}
+          onClick={() => {
+            setDropdownVisible(!dropdownVisible);
+          }}
+        />
+      </div>
+      <div
+        className={Styles["navbar-nav"]}
+        style={{
+          maxHeight: dropdownVisible ? "100vh" : "0px",
+        }}
+      >
+        <Link
+          href="/"
+          className={Styles["only-mobile"]}
+          onClick={() => {
+            setDropdownVisible(false);
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          href="/events"
+          onClick={() => {
+            setDropdownVisible(false);
+          }}
+        >
+          Events
+        </Link>
+        <Link
+          href="/campus-map"
+          onClick={() => {
+            setDropdownVisible(false);
+          }}
+        >
+          Campus Map
+        </Link>
+        <Link
+          href="/leaderboard"
+          onClick={() => {
+            setDropdownVisible(false);
+          }}
+        >
+          Leaderboard
+        </Link>
+        <Link
+          href="/schedule"
+          onClick={() => {
+            setDropdownVisible(false);
+          }}
+        >
+          Schedule
+        </Link>
+        <Link
+          href="/signin"
+          onClick={() => {
+            setDropdownVisible(false);
+          }}
+        >
+          Sign In
+        </Link>
       </div>
     </nav>
   );
