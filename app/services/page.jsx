695597315fac61ef42eca6b2cfd3fdb90164e770
Saharsh Agrawal <saharshagrawal@gmail.com>
2023-11-28 14:00:02 +0530

    services responsiveness sort

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -8,61 +8,58 @@ import Image from "next/image";
 
 import ServicesPlank from "../../public/assets/images/services/services.png";
 import FoodPlank from "../../public/assets/images/services/food.png";
-import FoodPlankMob from "../../public/assets/images/services/food_mob.png";
 import CyclesPlank from "../../public/assets/images/services/cycles.png";
+
+import ServicesPlankMob from "../../public/assets/images/services/services_mob.png";
+import FoodPlankMob from "../../public/assets/images/services/food_mob.png";
 import CyclesPlankMob from "../../public/assets/images/services/cycles_mob.png";
 
 const cookie = Cookie({ weight: "400", subsets: ["latin"] });
 
 export default function Services() {
-	const [width, setWidth] = useState(null);
-	const [mobSize, setMobSize] = useState(false);
+  //   const [mobSize, setMobSize] = useState(false);
 
-	useEffect(() => {
-		setWidth(window.innerWidth);
-	}, []);
+  //   useEffect(() => {
+  //     setMobSize(window.innerWidth < 1000 ? true : false);
+  //     function handleResize() {
+  //       setMobSize(window.innerWidth < 1000 ? true : false);
+  //     }
+  //     window.addEventListener("resize", handleResize);
+  //     return () => window.removeEventListener("resize", handleResize);
+  //   }, []);
 
-	useEffect(() => {
-		function handleResize() {
-			setWidth(window.innerWidth);
-		}
-		window.addEventListener("resize", handleResize);
-		return () => window.removeEventListener("resize", handleResize);
-	}, [width]);
-
-	useEffect(() => {
-		setMobSize(width < 1000 ? true : false);
-	}, [width]);
-
-	return (
-		<main className={cookie.className}>
-			<section className={Styles["main"]}>
-				<div className={Styles["plank-container"]}>
-					{!mobSize ? (
-						<div className={Styles["food"]}>
-							<Image src={FoodPlank} alt="Food" />
-							<h1>Food</h1>
-						</div>
-					) : null}
-					<div className={Styles["services"]}>
-						<Image src={ServicesPlank} alt="Services" />
-						<h1>Services</h1>
-					</div>
-					{mobSize ? (
-						<div className={Styles["food"]}>
-							<Image src={FoodPlankMob} alt="Food" />
-							<h1>Food</h1>
-						</div>
-					) : null}
-					<div className={Styles["cycles"]}>
-						<Image
-							src={mobSize ? CyclesPlankMob : CyclesPlank}
-							alt="Cycles"
-						/>
-						<h1>Cycles</h1>
-					</div>
-				</div>
-			</section>
-		</main>
-	);
+  return (
+    <main className={cookie.className}>
+      <section className={Styles["main"]}>
+        <div className={Styles["plank-container"]}>
+          <div className={Styles["food"]}>
+            <Image src={FoodPlank} alt="Food" />
+            <h1>Food</h1>
+          </div>
+          <div className={Styles["services"]}>
+            <Image src={ServicesPlank} alt="Services" />
+            <h1>Services</h1>
+          </div>
+          <div className={Styles["cycles"]}>
+            <Image src={CyclesPlank} alt="Cycles" />
+            <h1>Cycles</h1>
+          </div>
+        </div>
+        <div className={Styles["plank-container-mob"]}>
+          <div className={Styles["services"]}>
+            <Image src={ServicesPlankMob} alt="Services" />
+            <h1>Services</h1>
+          </div>
+          <div className={Styles["food"]}>
+            <Image src={FoodPlankMob} alt="Food" />
+            <h1>Food</h1>
+          </div>
+          <div className={Styles["cycles"]}>
+            <Image src={CyclesPlankMob} alt="Cycles" />
+            <h1>Cycles</h1>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
