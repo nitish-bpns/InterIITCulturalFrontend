a82208119a1a0832a7fa36aef4119ea6ab9b5971
nikhilrajani <nikhilrajani309@gmail.com>
2023-12-01 17:29:27 +0530

    added events details planks

--- app/events/[genre]/page.jsx
+++ app/events/[genre]/page.jsx
@@ -1,8 +1,20 @@
-import Styles from "@/styles/page.module.css";
+import Styles from "./genre.module.css";
 import { Satisfy } from "next/font/google";
 import BackButton from "@/components/BackButton";
 import data from "../data.json";
 import Link from "next/link";
+import Image from "next/image";
+import left1 from "../../../public/assets/images/events/genre/left_01.png";
+import left2 from "../../../public/assets/images/events/genre/left_02.png";
+import left3 from "../../../public/assets/images/events/genre/left_03.png";
+import left4 from "../../../public/assets/images/events/genre/left_04.png";
+import left5 from "../../../public/assets/images/events/genre/left_05.png";
+import left6 from "../../../public/assets/images/events/genre/left_06.png";
+import right1 from "../../../public/assets/images/events/genre/right_01.png";
+import right2 from "../../../public/assets/images/events/genre/right_02.png";
+import right3 from "../../../public/assets/images/events/genre/right_03.png";
+import right4 from "../../../public/assets/images/events/genre/right_04.png";
+import right5 from "../../../public/assets/images/events/genre/right_05.png";
 
 const satisfy = Satisfy({ weight: "400", subsets: ["latin"] });
 
@@ -26,12 +38,19 @@ export default function Genre({ params }) {
     >
       <section className={Styles["main"]}>
         <BackButton href={`/events`} />
-        <h1>{data[genre].properName}</h1>
-        {Object.keys(events).map((event, id) => (
-          <Link key={id} href={"/events/" + genre + "/" + event}>
-            <h2>{events[event]}</h2>
-          </Link>
-        ))}
+        {/* <h1>{data[genre].properName}</h1> */}
+        <div className={Styles["events-frame"]}>
+          {Object.keys(events).map((event, id) => (
+            <Link 
+              key={id} 
+              href={"/events/" + genre + "/" + event} 
+              className={Styles["event"]}
+            >
+              <Image src={left1} alt="event" className={Styles["plank"]}/>
+              <h2 className={Styles["event-name"]}>{events[event]}</h2>
+            </Link>
+          ))}
+        </div>
       </section>
     </main>
   );
