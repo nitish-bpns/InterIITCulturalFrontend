2a295e19d72941be3392caf81041e20ba19eedec
Saharsh Agrawal <saharshagrawal@gmail.com>
2023-12-15 04:09:01 +0530

    services page back button

--- app/services/cycles/page.jsx
+++ app/services/cycles/page.jsx
@@ -1,5 +1,5 @@
 import BackButton from "@/components/BackButton";
-import Styles from "../../../styles/page.module.css";
+import Styles from "@/styles/page.module.css";
 import { Cookie } from "next/font/google";
 
 const cookie = Cookie({ weight: "400", subsets: ["latin"] });
@@ -7,8 +7,8 @@ const cookie = Cookie({ weight: "400", subsets: ["latin"] });
 export default function Cycles() {
   return (
     <main className={cookie.className}>
+      <BackButton href="/services" />
       <section className={Styles["main"]}>
-        <BackButton href="/services" />
         <h1 className={Styles["heading"]}>Cycles</h1>
       </section>
     </main>
