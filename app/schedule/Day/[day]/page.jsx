5a4b23c72a688eee8ea2d242a7a8ed04e046c700
Saharsh Agrawal <saharshagrawal@gmail.com>
2023-12-18 00:06:49 +0530

    recent schedule data

--- app/schedule/Day/[day]/page.jsx
+++ app/schedule/Day/[day]/page.jsx
@@ -1,28 +1,31 @@
 import { Satisfy } from "next/font/google";
 import BackButton from "@/components/BackButton";
 import ErrorPage from "@/components/ErrorPage";
+
+import data from "@/data/schedule.json";
+
 import Styles from "@/styles/page.module.css";
 
 const satisfy = Satisfy({ weight: "400", subsets: ["latin"] });
 
 export default function Page({ params }) {
-  const day = params.day;
-  if (day > 3 || day < 0) return <ErrorPage statusCode={404} />;
+	const day = params.day;
+	if (!data[day]) return <ErrorPage statusCode={404} />;
 
-  return (
-    <main className={satisfy.className}>
-      <BackButton href={`/schedule`} />
-      <section className={Styles["main"]}>
-        <h1 className={Styles["heading"]}>Day {day}</h1>
-        <div
-          style={{
-            width: "90%",
-            minHeight: "80vh",
-            borderRadius: 15,
-            background: "rgba(0, 0, 0, 0.55)",
-          }}
-        ></div>
-      </section>
-    </main>
-  );
+	return (
+		<main className={satisfy.className}>
+			<BackButton href={`/schedule`} />
+			<section className={Styles["main"]}>
+				<h1 className={Styles["heading"]}>Day {day}</h1>
+				<div
+					style={{
+						width: "90%",
+						minHeight: "80vh",
+						borderRadius: 15,
+						background: "rgba(0, 0, 0, 0.55)",
+					}}
+				></div>
+			</section>
+		</main>
+	);
 }
