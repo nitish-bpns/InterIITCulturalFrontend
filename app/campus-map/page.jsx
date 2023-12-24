c12f1e4ed2cf3c5ff73f85fb71f6d70ea9a8437a
nkraj7266 <nkraj7266@gmail.com>
2023-12-24 06:27:38 +0530

    changed campus map fonts

--- app/campus-map/page.jsx
+++ app/campus-map/page.jsx
@@ -1,33 +1,35 @@
 import ModelView from "@/components/ModelView";
+import { Handlee } from "next/font/google";
 import Styles from "@/styles/page.module.css";
-import { Satisfy } from "next/font/google";
 import Image from "next/image";
 import PageStyles from "./page.module.css";
 
-const satisfy = Satisfy({ weight: "400", subsets: ["latin"] });
-
 export const metadata = {
-  title: "Campus Map",
+	title: "Campus Map",
 };
 
+const handlee = Handlee({ weight: "400", subsets: ["latin"] });
+
 export default function CampusMap() {
-  return (
-    <main className={satisfy.className}>
-      <section>
-        <h1 className={Styles["heading"]}>3D Map</h1>
-        <ModelView />
+	return (
+		<main className={handlee.className}>
+			<section>
+				<h1 className={PageStyles["heading"]}>3D Map</h1>
+				<ModelView />
 
-        <h3 className={PageStyles.head2}>Please right click and drag to Pan</h3>
-      </section>
+				<h3 className={PageStyles.head2}>
+					Please right click and drag to Pan
+				</h3>
+			</section>
 
-      <h1 className={Styles["heading"]}>2D Map</h1>
-      <Image
-        height={1000}
-        width={1000}
-        alt="2D Map"
-        className={PageStyles.map}
-        src="/assets/images/map.jpg"
-      />
-    </main>
-  );
+			<h1 className={PageStyles["heading"]}>2D Map</h1>
+			<Image
+				height={1000}
+				width={1000}
+				alt="2D Map"
+				className={PageStyles.map}
+				src="/assets/images/map.jpg"
+			/>
+		</main>
+	);
 }
